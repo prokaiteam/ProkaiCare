7ff733ff1f5e44298d1a0eafcc3d5d88a3115805
prokaiteam <prokai.team@gmail.com>
2025-07-24 15:23:31 +0530

    v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { fileURLToPath } from 'url'
+// import path from 'path'
+// import { fileURLToPath } from 'url'
 
 // 👉 ESM-compatible __dirname
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   base: '/ProkaiCare/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   server: {
     host: true,
     port: 5173,
